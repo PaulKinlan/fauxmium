9692e2bbd50826ce6b854302fd7012f739a14a35
Paul Kinlan <paul.kinlan@gmail.com>
2025-09-14 21:48:19 +0100

    feat: refactor index.js with new browser.js file

---
+++ browser.js
@@ -0,0 +1,87 @@
+import puppeteer from "puppeteer";
+
+function startBrowser(hostname, port) {
+  // Main function to launch Puppeteer and set up interception
+  (async () => {
+    const browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: null,
+      devtools: true,
+    });
+
+    browser.on("targetcreated", async (target) => {
+      if (target.type() === "page") {
+        const newPage = await target.page();
+        await setupRequestInterception(newPage);
+      }
+    });
+
+    const pages = await browser.pages();
+
+    for (const page of pages) {
+      await setupRequestInterception(page);
+    }
+  })();
+
+  async function setupRequestInterception(page) {
+    // Enable request interception
+    page.setDefaultNavigationTimeout(0);
+    await page.setRequestInterception(true);
+
+    // Listen for 'request' events
+    page.on("request", async (request) => {
+      // Check if the request URL matches a specific pattern
+      const resourceType = request.resourceType();
+      const url = request.url();
+      const method = request.method();
+      const headers = request.headers();
+
+      // Waiting on https://chromium-review.googlesource.com/c/chromium/src/+/6945075 - Thank you Andrey
+      const newHeaders = { ...headers, Referer: "" };
+
+      if (method === "GET" && request.isNavigationRequest()) {
+        const proxyUrl = `http://${hostname}:${port}/html?url=${encodeURIComponent(
+          url
+        )}&type=${resourceType}&headers=${encodeURIComponent(
+          JSON.stringify(headers)
+        )}`;
+        console.log(`Redirecting request from ${url} to ${proxyUrl}`);
+        console.log("Resource Type", resourceType);
+        console.log("BEFORE", request.interceptResolutionState());
+        // Continue the request with the new URL
+        await request.continue({
+          url: proxyUrl,
+          headers: newHeaders,
+        });
+
+        console.log("AFTER", request.interceptResolutionState());
+      } else {
+        if (resourceType === "image") {
+          const proxyUrl = `http://${hostname}:${port}/image?url=${encodeURIComponent(
+            url
+          )}&type=${resourceType}&headers=${encodeURIComponent(
+            JSON.stringify(headers)
+          )}`;
+          console.log(`Redirecting image request from ${url} to ${proxyUrl}`);
+          console.log("Resource Type", resourceType);
+          console.log("BEFORE", request.interceptResolutionState());
+
+          // new way
+
+          await request.continue({
+            url: proxyUrl,
+            headers: newHeaders,
+          });
+
+          console.log("AFTER", request.interceptResolutionState());
+          return;
+        } else {
+          // For all other requests, continue without changes
+          await request.respond("");
+        }
+      }
+    });
+  }
+}
+
+export { startBrowser };
--- index.js
+++ index.js
@@ -1,90 +1,8 @@
 #!/usr/bin/env node
 
 import "dotenv/config";
-import puppeteer from "puppeteer";
 import { startServer } from "./server.js";
-
-async function setupRequestInterception(page) {
-  // Enable request interception
-  page.setDefaultNavigationTimeout(0);
-  await page.setRequestInterception(true);
-
-  // Listen for 'request' events
-  page.on("request", async (request) => {
-    // Check if the request URL matches a specific pattern
-    const resourceType = request.resourceType();
-    const url = request.url();
-    const method = request.method();
-    const headers = request.headers();
-
-    // Waiting on https://chromium-review.googlesource.com/c/chromium/src/+/6945075 - Thank you Andrey
-    const newHeaders = { ...headers, Referer: "" };
-
-    if (method === "GET" && request.isNavigationRequest()) {
-      const proxyUrl = `http://${hostname}:${port}/html?url=${encodeURIComponent(
-        url
-      )}&type=${resourceType}&headers=${encodeURIComponent(
-        JSON.stringify(headers)
-      )}`;
-      console.log(`Redirecting request from ${url} to ${proxyUrl}`);
-      console.log("Resource Type", resourceType);
-      console.log("BEFORE", request.interceptResolutionState());
-      // Continue the request with the new URL
-      await request.continue({
-        url: proxyUrl,
-        headers: newHeaders,
-      });
-
-      console.log("AFTER", request.interceptResolutionState());
-    } else {
-      if (resourceType === "image") {
-        const proxyUrl = `http://${hostname}:${port}/image?url=${encodeURIComponent(
-          url
-        )}&type=${resourceType}&headers=${encodeURIComponent(
-          JSON.stringify(headers)
-        )}`;
-        console.log(`Redirecting image request from ${url} to ${proxyUrl}`);
-        console.log("Resource Type", resourceType);
-        console.log("BEFORE", request.interceptResolutionState());
-
-        // new way
-
-        await request.continue({
-          url: proxyUrl,
-          headers: newHeaders,
-        });
-
-        console.log("AFTER", request.interceptResolutionState());
-        return;
-      } else {
-        // For all other requests, continue without changes
-        await request.respond("");
-      }
-    }
-  });
-}
-
-// Main function to launch Puppeteer and set up interception
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-    devtools: true,
-  });
-
-  browser.on("targetcreated", async (target) => {
-    if (target.type() === "page") {
-      const newPage = await target.page();
-      await setupRequestInterception(newPage);
-    }
-  });
-
-  const pages = await browser.pages();
-
-  for (const page of pages) {
-    await setupRequestInterception(page);
-  }
-})();
+import { startBrowser } from "./browser.js";
 
 const hostname = "127.0.0.1";
 const port = 3001;
@@ -99,3 +17,4 @@ if (!API_KEY) {
 }
 
 startServer(hostname, port, API_KEY);
+startBrowser(hostname, port);
